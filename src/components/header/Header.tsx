4c498c4f982563ad6ae0bfb6e375baf67ca3e919
Kyoungho Eom <kheom0512@gmail.com>
2026-01-15 02:59:41 +0900

    feat: 헤더 푸터 타이포 적용

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="px-20 py-16">
-      <h1 className="title1_sb_18">SOPT 37th DEMODAY</h1>
+      <h1 className="head_b_20">SOPT 37th DEMODAY</h1>
     </header>
   );
 }
