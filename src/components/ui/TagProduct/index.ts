1421c09bb6ee8ba0d1edf8be97d3b8b858143d69
Kyoungho Eom <kheom0512@gmail.com>
2026-01-17 04:51:22 +0900

    feat: TagProduct 컴포넌트 추가

---
+++ src/components/ui/TagProduct/index.ts
@@ -0,0 +1,2 @@
+export { default } from './TagProduct';
+export type { TagProductProps } from './TagProduct';
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -10,3 +10,6 @@ export { default as Tabs } from './Tabs/Tabs';
 export type { TabsProps, TabsValue } from './Tabs/Tabs';
 
 export { default as NavBottomBtn } from './navBottomBtn/NavBottomBtn';
+
+export { default as TagProduct } from './TagProduct/TagProduct';
+export type { TagProductProps } from './TagProduct/TagProduct';
