7a788231827eb6ff51c38a57151be5be785464f8
Kyoungho Eom <kheom0512@gmail.com>
2026-01-17 06:01:02 +0900

    feat: 홈 메인 브랜딩 화면 구현

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,7 +1,20 @@
 export default function Footer() {
   return (
-    <footer className="px-20 py-16">
-      <p className="caption_r_10 text-[var(--color-white-70)]">© DIVE SOPT</p>
+    <footer className="flex w-full flex-col gap-[12px] bg-[var(--color-gray-900)] px-[16px] pt-[24px] pb-[12px]">
+      <div className="grid h-[14px] w-[40px] place-items-center rounded-[2px] bg-[var(--color-gray-800)]">
+        <p className="caption_r_10 text-[var(--color-gray-400)]">SOPT</p>
+      </div>
+
+      <p className="title_m_12 text-[var(--color-gray-300)]">
+        2026 37th DIVE SOPT DEMODAY
+        <br />
+        System Update: SUNRISE
+      </p>
+      <p className="caption_r_10 text-[var(--color-gray-300)]">
+        SOPT (솝트, 대학생연합 IT벤처창업 동아리)
+        <br />
+        Copyright©2026.SOPT.All rights reserved.
+      </p>
     </footer>
   );
 }
