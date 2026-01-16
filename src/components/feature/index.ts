da46e1b54d98610702dffacbb4d7065272310f53
Kyoungho Eom <kheom0512@gmail.com>
2026-01-17 04:52:24 +0900

    feat: 프로덕트 카드 컴포넌트 추가

--- src/components/feature/index.ts
+++ src/components/feature/index.ts
@@ -1,2 +1,8 @@
 export { default as Divider } from './section-header/Divider';
 export { default as TagMain } from './section-header/TagMain';
+
+export { default as ProductCard } from './product-card/ProductCard';
+export type {
+  ProductCardProps,
+  ProductCardState,
+} from './product-card/ProductCard';
