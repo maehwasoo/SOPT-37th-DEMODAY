eb09954ac768e4c7094d49ae383be048fe4433f7
Kyoungho Eom <kheom0512@gmail.com>
2026-01-18 02:57:12 +0900

    fix: 홈 배너와 로고 이미지를 적용

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
+
 export default function Footer() {
   return (
     <footer className="flex w-full flex-col gap-[12px] bg-[var(--color-gray-900)] pt-[24px] pr-0 pb-[12px] pl-[16px]">
-      <div className="grid h-[14px] w-[40px] place-items-center rounded-[2px] bg-[var(--color-gray-800)]">
-        <p className="caption_r_10 text-[var(--color-gray-400)]">SOPT</p>
+      <div className="relative h-[14px] w-[40px]">
+        <Image
+          src="/assets/figma/main/logo_sopt_black.svg"
+          alt="SOPT"
+          fill
+          className="object-contain"
+        />
       </div>
 
       <p className="title_m_12 text-[var(--color-gray-300)]">
