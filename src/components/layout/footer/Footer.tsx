c6761a6cc1940f9ab86c2d9064cd787dab28b98e
Kyoungho Eom <kheom0512@gmail.com>
2026-01-17 14:16:00 +0900

    fix: 홈 메인 간격 정렬 일치

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex w-full flex-col gap-[12px] bg-[var(--color-gray-900)] px-[16px] pt-[24px] pb-[12px]">
+    <footer className="flex w-full flex-col gap-[12px] bg-[var(--color-gray-900)] pt-[24px] pr-0 pb-[12px] pl-[16px]">
       <div className="grid h-[14px] w-[40px] place-items-center rounded-[2px] bg-[var(--color-gray-800)]">
         <p className="caption_r_10 text-[var(--color-gray-400)]">SOPT</p>
       </div>
