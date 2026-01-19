86742b1006416cc1b40b3a73870db592f8f50994
Kyoungho Eom <kheom0512@gmail.com>
2026-01-20 02:52:58 +0900

    fix: 리플렛에서 Footer 숨김 처리

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,4 +1,14 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
 export default function Footer() {
+  const pathname = usePathname();
+
+  if (pathname.startsWith('/leaflet')) {
+    return null;
+  }
+
   return (
     <footer className="px-20 py-16">
       <p className="caption_r_10 text-[var(--color-white-70)]">© DIVE SOPT</p>
