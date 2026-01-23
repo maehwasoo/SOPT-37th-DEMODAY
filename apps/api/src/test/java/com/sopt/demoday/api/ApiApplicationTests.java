78c5605b3bcb59092089eced9d6a6fae451d4052
Kyoungho Eom <kheom0512@gmail.com>
2026-01-23 12:53:35 +0900

    test: CI에서 Testcontainers 기동 보강

--- apps/api/src/test/java/com/sopt/demoday/api/ApiApplicationTests.java
+++ apps/api/src/test/java/com/sopt/demoday/api/ApiApplicationTests.java
@@ -3,9 +3,7 @@ package com.sopt.demoday.api;
 import com.sopt.demoday.api.support.IntegrationTestSupport;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 class ApiApplicationTests extends IntegrationTestSupport {
 
 	@Test
--- apps/api/src/test/java/com/sopt/demoday/api/support/IntegrationTestSupport.java
+++ apps/api/src/test/java/com/sopt/demoday/api/support/IntegrationTestSupport.java
@@ -4,14 +4,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest
 public abstract class IntegrationTestSupport {
 
-	@Container
 	static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
 		.withDatabaseName("demoday")
 		.withUsername("demoday")
@@ -19,6 +15,10 @@ public abstract class IntegrationTestSupport {
 
 	@DynamicPropertySource
 	static void overrideProperties(final DynamicPropertyRegistry registry) {
+		if (!postgres.isRunning()) {
+			postgres.start();
+		}
+
 		registry.add("spring.datasource.url", postgres::getJdbcUrl);
 		registry.add("spring.datasource.username", postgres::getUsername);
 		registry.add("spring.datasource.password", postgres::getPassword);
